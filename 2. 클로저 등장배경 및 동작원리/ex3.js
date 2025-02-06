1690133b66b2551a130231f5897ba34a6004a5df
Cho hyun-sik <78861124+1223v@users.noreply.github.com>
2025-02-06 09:34:48 +0900

    Create ex3.js

---
+++ 2. 클로저 등장배경 및 동작원리/ex3.js
@@ -0,0 +1,14 @@
+const popular_music = "hip-hop";
+
+function body() {
+    const popular_music = "Rock & Roll";
+
+    const spirit = function () {
+        console.log(popular_music);
+    };
+
+    return spirit;
+}
+
+const hyunsik = body();
+hyunsik();
